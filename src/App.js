4ba7b9328823c0ac6ca62a57504e4670570f025d
Brent Ingram <bingr001@users.noreply.github.com>
2019-10-07 18:17:36 -0700

    most functionality working

--- src/App.js
+++ src/App.js
@@ -1,29 +1,68 @@
 import React, { Component } from 'react';
 import './App.css';
+import $ from 'jquery'; 
 import MovieRow from './MovieRow.js'
 
 class App extends Component {
   constructor(props) {
     super(props) 
-      console.log("This is my initializer")
+    this.state = {}  
+    // console.log("This is my initializer")
 
-      const movies = [
-        { id: 0, poster_src: "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_.jpg" , title: "Avengers: Infinity War", overview: "Lorem ipsum dolor amet taiyaki marfa schlitz sriracha bitters disrupt small batch. Distillery la croix cred, hot chicken lyft tumblr fixie. Edison bulb forage disrupt brooklyn wolf meditation. Flexitarian mlkshk chia synth crucifix irony tumblr taiyaki squid shaman chicharrones gluten-free portland farm-to-table. Four dollar toast quinoa tousled, XOXO asymmetrical poke raw denim pug succulents chartreuse affogato organic. Pitchfork pok pok street art cray. Beard kogi vexillologist retro la croix trust fund pour-over organic keffiyeh asymmetrical artisan, normcore mumblecore typewriter." },
-        { id: 1, poster_src: "https://images-na.ssl-images-amazon.com/images/I/71niXI3lxlL._SY606_.jpg", title: "Avengers: Endgame", overview: "Lorem ipsum dolor amet taiyaki marfa schlitz sriracha bitters disrupt small batch. Distillery la croix cred, hot chicken lyft tumblr fixie. Edison bulb forage disrupt brooklyn wolf meditation. Flexitarian mlkshk chia synth crucifix irony tumblr taiyaki squid shaman chicharrones gluten-free portland farm-to-table. Four dollar toast quinoa tousled, XOXO asymmetrical poke raw denim pug succulents chartreuse affogato organic. Pitchfork pok pok street art cray. Beard kogi vexillologist retro la croix trust fund pour-over organic keffiyeh asymmetrical artisan, normcore mumblecore typewriter." },
-      ]
+      // const movies = [
+      //   { id: 0, poster_src: "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_.jpg" , title: "Avengers: Infinity War", overview: "Lorem ipsum dolor amet taiyaki marfa schlitz sriracha bitters disrupt small batch. Distillery la croix cred, hot chicken lyft tumblr fixie. Edison bulb forage disrupt brooklyn wolf meditation. Flexitarian mlkshk chia synth crucifix irony tumblr taiyaki squid shaman chicharrones gluten-free portland farm-to-table. Four dollar toast quinoa tousled, XOXO asymmetrical poke raw denim pug succulents chartreuse affogato organic. Pitchfork pok pok street art cray. Beard kogi vexillologist retro la croix trust fund pour-over organic keffiyeh asymmetrical artisan, normcore mumblecore typewriter." },
+      //   { id: 1, poster_src: "https://images-na.ssl-images-amazon.com/images/I/71niXI3lxlL._SY606_.jpg", title: "Avengers: Endgame", overview: "Lorem ipsum dolor amet taiyaki marfa schlitz sriracha bitters disrupt small batch. Distillery la croix cred, hot chicken lyft tumblr fixie. Edison bulb forage disrupt brooklyn wolf meditation. Flexitarian mlkshk chia synth crucifix irony tumblr taiyaki squid shaman chicharrones gluten-free portland farm-to-table. Four dollar toast quinoa tousled, XOXO asymmetrical poke raw denim pug succulents chartreuse affogato organic. Pitchfork pok pok street art cray. Beard kogi vexillologist retro la croix trust fund pour-over organic keffiyeh asymmetrical artisan, normcore mumblecore typewriter." },
+      // ]
     
 
-      var movieRows = []
-      movies.forEach((movie) => {
-        console.log(movie.title)
-        const movieRow = <MovieRow movie={movie} />
+      // var movieRows = []
+      // movies.forEach((movie) => {
+      //   console.log(movie.title)
+      //   const movieRow = <MovieRow movie={movie} />
  
-        movieRows.push(movieRow)
-      })
+      //   movieRows.push(movieRow)
+      // })
 
-      this.state = {rows: movieRows}
+      // this.state = {rows: movieRows}
+      this.performSearch("man")
   }
 
+  performSearch(searchTerm) {
+    console.log("perform search term")
+    const urlString = "https://api.themoviedb.org/3/search/movie?api_key=bd57c465124b01d880b8dd4d14066cc6&query=" + searchTerm
+
+
+    $.ajax({
+      url: urlString,
+      success: (searchResults) => {
+        console.log("Fetched successfully")
+        //console.log(searchResults)
+        const results = searchResults.results
+        //console.log(results[0])
+
+        var movieRows = []
+
+        results.forEach((movie) => {
+          
+          movie.poster_src= "https://image.tmdb.org/t/p/w185" + movie.poster_path
+          console.log(movie.poster_path)
+          const movieRow = <MovieRow key={movie.id} movie={movie}/>
+          movieRows.push(movieRow)
+        })
+        this.setState({rows: movieRows})
+      },
+      error: (xhr, status, err) => {
+        console.log("Failed to fetch data")
+      }
+    })
+  }
+
+  searchChangeHandler(event) {
+    console.log(event.target.value)
+    const boundObject = this
+    const searchTerm = event.target.value
+    boundObject.performSearch(searchTerm)
+  }
 
 render() {
   return (
@@ -50,7 +89,7 @@ render() {
         paddingTop: 8,
         paddingBOttom: 8,
         paddingLeft: 16
-      }} placeholder="Enter Search Term" />
+      }} onChange={this.searchChangeHandler.bind(this)} placeholder="Enter Search Term" />
 
       {this.state.rows}
 
--- src/MovieRow.js
+++ src/MovieRow.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
 class MovieRow extends React.Component {
+    viewMovie(){
+        // console.log("trying to view movie")
+        // console.log(this.props.movie.title)
+        const url = "https:/www.themoviedb.org/movie/" + this.props.movie.id
+        window.location.href = url
+    }
+
     render() {
         return <table key={this.props.movie.id}>
             <tbody>
@@ -12,8 +19,11 @@ class MovieRow extends React.Component {
                             alt="poster" width="120" src={this.props.movie.poster_src} />
                     </td>
                     <td>
-                        {this.props.movie.title}
+                        <h3>
+                            {this.props.movie.title}
+                        </h3>
                         <p>{this.props.movie.overview}</p>
+                        <input type="button" onClick={this.viewMovie.bind(this)} value="View"/>
                     </td>
                 </tr>
             </tbody>
